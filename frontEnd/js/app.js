f4a2ba0678da08ca14e061e5f4217d3784bca355
Maryam <maryamolabisi02@gmail.com>
2016-01-15 11:34:21 +0100

    feat(root): add frontEnd/ folder

---
+++ frontEnd/js/app.js
@@ -0,0 +1,9 @@
+angular.module('myApp', ['uiRouter'])
+.config()
+.directive()
+.service()
+.controller('mainCtrl', ['$scope', function($scope){
+  $scope.test = 'test';
+}]);
+
+//check if it works 
