4d33da47bbf0be84c338041ed66122a54e47b6a9
Maryam <maryamolabisi02@gmail.com>
2016-01-20 22:37:28 +0100

    feat(root): modify .gitignore

--- public/js/app.js
+++
@@ -1,9 +0,0 @@
-angular.module('myApp', ['uiRouter'])
-.config()
-.directive()
-.service()
-.controller('mainCtrl', ['$scope', function($scope){
-  $scope.test = 'test';
-}]);
-
-//check if it works 
