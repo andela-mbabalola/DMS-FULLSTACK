bad2c1f0743fed640e8e3195a1256107345e9227
andela-mbabalola <maryam.babalola@andela.com>
2016-02-25 18:23:03 +0100

    Fend and Bend test [Refers to #111384210]

--- frontEnd/js/controllers/home.js
+++
@@ -1,6 +0,0 @@
-angular.module('Doccy.controllers')
-  .controller('HomeCtrl', ['$scope',
-    ($scope)=> {
-        $scope.jumbotron = 'images/jumbotron.jpeg';
-    }
-  ]);
--- frontEnd/js/services/limit.js
+++
@@ -1,9 +0,0 @@
-angular.module('Doccy.filters')
-.filter('limitHtml', function() {
-    return function(text, limit) {
-        var changedString = String(text).replace(/<[^>]+>/gm, '');
-        //var length = changedString.length;
-        return changedString.length > limit ?
-        changedString.substr(0, limit - 1) : changedString; 
-    };
-});
